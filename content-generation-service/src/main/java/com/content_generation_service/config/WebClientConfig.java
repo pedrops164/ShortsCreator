9f0adf31a13ff6a27bcc2c32ab82fe3e5c440395
pedrops164 <ordeppsousa2@gmail.com>
2025-09-16 15:04:54 +0100

    Configured web client so that http connections are not made on zombie connection pools

--- content-generation-service/src/main/java/com/content_generation_service/config/WebClientConfig.java
+++ content-generation-service/src/main/java/com/content_generation_service/config/WebClientConfig.java
@@ -1,27 +1,20 @@
-package com.content_storage_service.config;
-
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.web.reactive.function.client.WebClient;
+package com.content_generation_service.config;
 
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.resources.ConnectionProvider;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 public class WebClientConfig {
-
-    private final AppProperties appProperties;
-
-    public WebClientConfig(AppProperties appProperties) {
-        this.appProperties = appProperties;
-    }
 
     @Bean
     public HttpClient httpClient() {
@@ -48,12 +41,5 @@ public class WebClientConfig {
     public WebClient.Builder webClientBuilder(HttpClient httpClient) {
         return WebClient.builder()
             .clientConnector(new ReactorClientHttpConnector(httpClient));
-    }
-
-    @Bean
-    public WebClient paymentServiceWebClient() {
-        return WebClient.builder()
-                .baseUrl(appProperties.getServices().getPaymentService().getUrl())
-                .build();
     }
 }
